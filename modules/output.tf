0aad43565ce873b1d22719665169a4150ade9c49
ajaypatil352 <117426390+ajaypatil352@users.noreply.github.com>
2023-06-20 12:54:45 +0530

    Update output.tf

--- modules/output.tf
+++ modules/output.tf
@@ -1,1 +1,14 @@
+output "alb_arn" {
+  description = "ARN of the Application Load Balancer"
+  value       = aws_lb.alb.arn
+}
 
+output "target_group_arn" {
+  description = "ARN of the target group"
+  value       = aws_lb_target_group.tg.arn
+}
+
+output "listener_arn" {
+  description = "ARN of the ALB listener"
+  value       = aws_lb_listener.alb_listener.arn
+}
