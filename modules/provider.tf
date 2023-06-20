af1dfca2bf7a328674ec51d160f4f8e83f1834a7
ajaypatil352 <117426390+ajaypatil352@users.noreply.github.com>
2023-06-20 12:50:35 +0530

    Update provider.tf

--- modules/provider.tf
+++ modules/provider.tf
@@ -2,14 +2,14 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "5.4.0"
     }
   }
 }
 
 provider "aws" {
-  region     = var.aws_region
-  access_key     = var.aws_access_key
+  region          = "ap-south-1"
+  access_key      = var.aws_access_key
   secret_key      = var.aws_secret_key
 }
