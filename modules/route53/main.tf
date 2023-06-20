95165369ee70d159b2d0d229e11b5cf07e510271
ajaypatil352 <117426390+ajaypatil352@users.noreply.github.com>
2023-06-20 16:00:31 +0530

    Update main.tf

--- modules/route53/main.tf
+++ modules/route53/main.tf
@@ -1,5 +1,5 @@
 resource "aws_route53_zone" "route53_zone" {
-  name = "aws_route53.com"  
+  name = "aws_route53.com"  # Replace with your desired domain name
 }
 
 resource "aws_route53_record" "route53_record" {
@@ -10,18 +10,6 @@ resource "aws_route53_record" "route53_record" {
 
   records = [
     aws_instance.example_ec2_instance.public_ip  # Replace with the appropriate resource, e.g., an EC2 instance
-  ]
-}
-
-# Create a CNAME record pointing to another domain
-resource "aws_route53_record" "example_cname" {
-  zone_id = aws_route53_zone.example_zone.zone_id
-  name    = "blog.example.com"  # Replace with your desired record name
-  type    = "CNAME"
-  ttl     = 300  # Replace with your desired TTL value
-
-  records = [
-    "blog.anotherdomain.com"  # Replace with the target domain you want to point to
   ]
 }
 
