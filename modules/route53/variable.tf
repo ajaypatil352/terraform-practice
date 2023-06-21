2492dde571f70a73cb0d5dc5d29afaf64091eedb
ajaypatil352 <117426390+ajaypatil352@users.noreply.github.com>
2023-06-21 15:30:06 +0530

    Update variable.tf

--- modules/route53/variable.tf
+++ modules/route53/variable.tf
@@ -1,1 +1,55 @@
+variable "domain_name" {
+  description = "The domain name for the hosted zone"
+  type        = string
+}
+
+variable "routing_policy_type" {
+  description = "The type of routing policy"
+  type        = string
+  default     = "weighted"  # Specify the desired routing policy type here (weighted, failover, geolocation, latency)
+}
+
+variable "record_name" {
+  description = "The name for the DNS record"
+  type        = string
+}
+
+variable "weighted_routing_policy" {
+  description = "The weighted routing policy configuration"
+  type        = object({
+    weight     = number
+    set_id     = string
+  })
+  default     = null
+}
+
+variable "failover_routing_policy" {
+  description = "The failover routing policy configuration"
+  type        = object({
+    set_id        = string
+    health_check  = string
+    failover_type = string
+  })
+  default     = null
+}
+
+variable "geolocation_routing_policy" {
+  description = "The geolocation routing policy configuration"
+  type        = object({
+    set_id       = string
+    country_code = string
+  })
+  default     = null
+}
+
+variable "latency_routing_policy" {
+  description = "The latency routing policy configuration"
+  type        = object({
+    set_id       = string
+    region       = string
+  })
+  default     = null
+}
+
+
 
