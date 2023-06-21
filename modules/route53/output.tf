5740fff7f3c5d17dd7d52ba45f697661765907ef
ajaypatil352 <117426390+ajaypatil352@users.noreply.github.com>
2023-06-21 15:39:37 +0530

    Update output.tf

--- modules/route53/output.tf
+++ modules/route53/output.tf
@@ -1,1 +1,15 @@
 
+output "hosted_zone_id" {
+  description = "The ID of the created Route 53 hosted zone"
+  value       = aws_route53_zone.example.zone_id
+}
+
+output "routing_policy_record_name" {
+  description = "The name of the created DNS record based on the routing policy type"
+  value       = aws_route53_record.routing_policy.name
+}
+
+output "routing_policy_type" {
+  description = "The type of routing policy used"
+  value       = var.routing_policy_type
+}
