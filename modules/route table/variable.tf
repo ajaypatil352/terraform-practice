33a1a35b5f43364cc4050032900391304bf8f155
ajaypatil352 <117426390+ajaypatil352@users.noreply.github.com>
2023-06-22 12:50:28 +0530

    Update variable.tf

--- modules/route table/variable.tf
+++ modules/route table/variable.tf
@@ -1,1 +1,14 @@
-.
+variable "vpc_cidr_block" {
+  description = "CIDR block for the VPC"
+  default     = "10.0.0.0/16"
+}
+
+variable "subnet_cidr_block" {
+  description = "CIDR block for the subnet"
+  default     = "10.0.1.0/24"
+}
+
+variable "route_cidr_block" {
+  description = "CIDR block for the route"
+  default     = "0.0.0.0/0"
+}
