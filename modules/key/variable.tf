d9918f1e9b78683a6e23da08777c39c42552f7fe
ajaypatil352 <117426390+ajaypatil352@users.noreply.github.com>
2023-06-22 12:17:12 +0530

    Update variable.tf

--- modules/key/variable.tf
+++ modules/key/variable.tf
@@ -1,1 +1,12 @@
+variable "key_pair_name" {
+  description = "Name of the key pair"
+  type        = string
+  default     = "my-key-pair"
+}
+
+variable "public_key_path" {
+  description = "Path to the public key file"
+  type        = string
+  default     = "~/.ssh/id_rsa.pub"
+}
 
