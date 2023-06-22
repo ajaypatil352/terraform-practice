0036facd2e3b97ab2dbb5989af191d11b76760ae
ajaypatil352 <117426390+ajaypatil352@users.noreply.github.com>
2023-06-22 12:16:05 +0530

    Update main.tf

--- modules/key/main.tf
+++ modules/key/main.tf
@@ -1,1 +1,8 @@
+# Create key pair
+resource "aws_key_pair" "my_key_pair" {
+  key_name   = "my-key-pair"  # Replace with your desired key pair name
+  public_key = file("~/.ssh/id_rsa.pub")  # Replace with the path to your public key file
+}
+
+
 
