40c362cc78bfa4c89b09dd2718d4c3a8e5de915c
ajaypatil352 <117426390+ajaypatil352@users.noreply.github.com>
2023-06-22 12:56:11 +0530

    Update main.tf

--- modules/route table/main.tf
+++ modules/route table/main.tf
@@ -1,12 +1,12 @@
 # VPC configuration
 resource "aws_vpc" "my_vpc" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block = var.vpc_cidr_block
 }
 
 # Subnet configuration
 resource "aws_subnet" "my_subnet" {
   vpc_id = aws_vpc.my_vpc.id
-  cidr_block = "10.0.1.0/24"
+  cidr_block = var.subnet_cidr_block
 }
 
 # Route table configuration
@@ -14,7 +14,7 @@ resource "aws_route_table" "my_route_table" {
   vpc_id = aws_vpc.my_vpc.id
 
   route {
-    cidr_block = "0.0.0.0/0"
+    cidr_block = var.route_cidr_block
     gateway_id = aws_internet_gateway.my_internet_gateway.id
   }
 }
