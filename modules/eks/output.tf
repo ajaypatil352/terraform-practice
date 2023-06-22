e970dabe42941b73168a90d3e4078c8dd83e272e
ajaypatil352 <117426390+ajaypatil352@users.noreply.github.com>
2023-06-22 15:54:25 +0530

    Update output.tf

--- modules/eks/output.tf
+++ modules/eks/output.tf
@@ -1,1 +1,20 @@
+output "ecs_cluster_id" {
+  description = "ID of the ECS cluster"
+  value       = aws_ecs_cluster.my_cluster.id
+}
+
+output "task_definition_arn" {
+  description = "ARN of the ECS task definition"
+  value       = aws_ecs_task_definition.my_task_definition.arn
+}
+
+output "service_id" {
+  description = "ID of the ECS service"
+  value       = aws_ecs_service.my_service.id
+}
+
+output "service_name" {
+  description = "Name of the ECS service"
+  value       = aws_ecs_service.my_service.name
+}
 
