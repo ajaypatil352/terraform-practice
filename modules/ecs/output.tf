3a6bb8626d789c3665e0c3b663e6232bc69c1b74
ajaypatil352 <117426390+ajaypatil352@users.noreply.github.com>
2023-07-01 11:56:42 +0530

    Update output.tf

--- modules/ecs/output.tf
+++ modules/ecs/output.tf
@@ -12,9 +12,9 @@ output "service_name" {
   description = "Name of the ECS service"
   value       = aws_ecs_service.my_service.name
 }
-output "service_name" {
-  description = "Name of the ECS service"
-  value       = aws_ecs_service.my_service.name
-}
 
+output "service_arn" {
+  description = "ARN of the ECS service"
+  value       = aws_ecs_service.my_service.id
+}
 
