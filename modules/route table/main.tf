703ab37b0eb3ca631c57bb06797785eb8d20bb8c
ajaypatil352 <117426390+ajaypatil352@users.noreply.github.com>
2023-07-01 09:31:15 +0530

    Update main.tf

--- modules/route table/main.tf
+++ modules/route table/main.tf
@@ -35,3 +35,9 @@ resource "aws_internet_gateway" "my_internet_gateway" {
     Name = "MyInternetGateway"
   }
 }
+
+# Associate the route table with the subnet
+resource "aws_route_table_association" "my_route_table_association" {
+  subnet_id = aws_subnet.my_subnet.id
+  route_table_id = aws_route_table.my_route_table.id
+}
