46f4587c12b95c5885b1e0e96b407da55d03c0dc
ajaypatil352 <117426390+ajaypatil352@users.noreply.github.com>
2023-07-01 14:04:30 +0530

    Update variable.tf

--- modules/ecs/variable.tf
+++ modules/ecs/variable.tf
@@ -29,17 +29,7 @@ variable "container_port" {
   default     = 80
 }
 
-variable "cpu_units" {
-  description = "CPU units for the task"
-  type        = number
-  default     = 1
-}
 
-variable "memory_mb" {
-  description = "Memory in MB for the task"
-  type        = number
-  default     = 512
-}
 
 variable "service_name" {
   description = "Name of the ECS service"
