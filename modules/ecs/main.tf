f7ae9dc2649c0194bfa133d9e62d0ed5c8952238
ajaypatil352 <117426390+ajaypatil352@users.noreply.github.com>
2023-07-01 11:16:43 +0530

    Create main.tf

---
+++ modules/ecs/main.tf
@@ -0,0 +1,101 @@
+# Create an ECS cluster
+resource "aws_ecs_cluster" "my_cluster" {
+  name = "my-cluster"  # Update with your desired cluster name
+}
+
+# Create a task definition
+resource "aws_ecs_task_definition" "my_task_definition" {
+  family                = "my-task-def"  # Update with your desired task definition family
+  execution_role_arn    = "arn:aws:iam::123456789012:role/ecsTaskExecutionRole"  # Update with the ARN of your execution role
+  network_mode          = "awsvpc"
+  requires_compatibilities = ["FARGATE"]  # Update with the launch type compatibility
+
+  container_definitions = <<DEFINITION
+[
+  {
+    "name": "my-container",
+    "image": "nginx:latest",  # Update with your desired container image
+    "portMappings": [
+      {
+        "containerPort": 80,  # Update with your container's port
+        "protocol": "tcp"
+      }
+    ],
+    "cpu": 1,  # Update with the desired CPU units
+    "memory": 512,  # Update with the desired memory in MB
+    "essential": true
+  }
+]
+DEFINITION
+}
+
+# Create an ECS service
+resource "aws_ecs_service" "my_service" {
+  name            = "my-service"  # Update with your desired service name
+  cluster         = aws_ecs_cluster.my_cluster.id
+  task_definition = aws_ecs_task_definition.my_task_definition.arn
+  desired_count   = 2  # Update with your desired number of tasks
+
+  deployment_controller {
+    type = "ECS"
+  }
+
+  network_configuration {
+    subnets         = [aws_vpc.my_vpc.public_subnets[0]]  # Update with your desired subnets
+    security_groups = [aws_security_group.my_security_group.id]
+  }
+}
+
+# Create a VPC for the ECS cluster
+resource "aws_vpc" "my_vpc" {
+  cidr_block = "10.0.0.0/16"  # Update with your desired VPC CIDR block
+
+  tags = {
+    Name = "my-vpc"  # Update with your desired VPC name
+  }
+}
+
+# Create an internet gateway
+resource "aws_internet_gateway" "my_gateway" {
+  vpc_id = aws_vpc.my_vpc.id
+}
+
+# Create a route table
+resource "aws_route_table" "my_route_table" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.my_gateway.id
+  }
+}
+
+# Associate the route table with the VPC's main route table
+resource "aws_main_route_table_association" "my_association" {
+  vpc_id         = aws_vpc.my_vpc.id
+  route_table_id = aws_route_table.my_route_table.id
+}
+
+# Create a security group for the ECS cluster
+resource "aws_security_group" "my_security_group" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  ingress {
+    from_port   = 80  # Update with your desired port
+    to_port     = 80  # Update with your desired port
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# Output the ECS cluster's ARN
+output "ecs_cluster_arn" {
+  value = aws_ecs_cluster.my_cluster.arn
+}
