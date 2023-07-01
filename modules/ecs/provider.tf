fb3e0a930e64ebfff376b4a38855ca20680778b6
ajaypatil352 <117426390+ajaypatil352@users.noreply.github.com>
2023-07-01 11:39:12 +0530

    Update provider.tf

--- modules/ecs/provider.tf
+++ modules/ecs/provider.tf
@@ -1,4 +1,3 @@
-
 terraform {
   required_providers {
     aws = {
@@ -8,7 +7,7 @@ terraform {
   }
 }
 
-provider "aws" {
+provider "aws"{
   region          = "ap-south-1"
   access_key      = var.aws_access_key
   secret_key      = var.aws_secret_key
