572e055c7e1351955c89e27e9b262480ac3fcb49
ajaypatil352 <117426390+ajaypatil352@users.noreply.github.com>
2023-07-01 11:55:39 +0530

    Update output.tf

--- modules/ecs/output.tf
+++ modules/ecs/output.tf
@@ -8,18 +8,13 @@ output "task_definition_arn" {
   value       = aws_ecs_task_definition.my_task_definition.arn
 }
 
-output "service_arn" {
-  description = "ARN of the ECS service"
-  value       = aws_ecs_service.my_service.arn
+output "service_name" {
+  description = "Name of the ECS service"
+  value       = aws_ecs_service.my_service.name
 }
-
 output "service_name" {
   description = "Name of the ECS service"
   value       = aws_ecs_service.my_service.name
 }
 
-output "service_url" {
-  description = "URL of the ECS service"
-  value       = aws_ecs_service.my_service.load_balancer.first.hostname
-}
 
