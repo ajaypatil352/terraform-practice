faf3a18d4968750082febb33ef459c224e86763c
ajaypatil352 <117426390+ajaypatil352@users.noreply.github.com>
2023-07-03 12:20:28 +0530

    Update main.tf

--- modules/ecs/main.tf
+++ modules/ecs/main.tf
@@ -1,51 +1,3 @@
-# Create a VPC for the ECS cluster
-resource "aws_vpc" "my_vpc" {
-  cidr_block = var.vpc_cidr_block
-
-  tags = {
-    Name = var.vpc_name
-  }
-}
-# Create a security group for the ECS cluster
-resource "aws_security_group" "my_security_group" {
-  vpc_id = aws_vpc.my_vpc.id
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-# Create an internet gateway
-resource "aws_internet_gateway" "my_gateway" {
-  vpc_id = aws_vpc.my_vpc.id
-}
-
-# Create a route table
-resource "aws_route_table" "my_route_table" {
-  vpc_id = aws_vpc.my_vpc.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.my_gateway.id
-  }
-}
-
-# Associate the route table with the VPC's main route table
-resource "aws_main_route_table_association" "my_association" {
-  vpc_id         = aws_vpc.my_vpc.id
-  route_table_id = aws_route_table.my_route_table.id
-}
-
-
 
 # Create an ECS cluster
 resource "aws_ecs_cluster" "my_cluster" {
@@ -88,9 +40,12 @@ resource "aws_ecs_service" "my_service" {
   }
 
   network_configuration {
-    subnets         = [var.subnet_id]
-    security_groups = [aws_security_group.my_security_group.id]
+    subnets         = var.subnet_id
+    security_groups = var.security_groups
   }
 }
+
+
+
 
 
