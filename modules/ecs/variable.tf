e965da95f527f4d82141020fe1e2b63e5c56426f
ajaypatil352 <117426390+ajaypatil352@users.noreply.github.com>
2023-07-03 12:06:29 +0530

    Update variable.tf

--- modules/ecs/variable.tf
+++ modules/ecs/variable.tf
@@ -72,24 +72,7 @@ variable "gateway_id" {
   type        = string
   default     = ""
 }
-# Create a security group for the ECS cluster
-resource "aws_security_group" "my_security_group" {
-  vpc_id = aws_vpc.my_vpc.id
 
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
 
 variable "aws_access_key" {}
 variable "aws_secret_key" {}
