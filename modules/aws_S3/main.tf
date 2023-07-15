dd544ead7d787fad64a03687f891e4ec74309536
ajaypatil352 <117426390+ajaypatil352@users.noreply.github.com>
2023-07-15 14:19:16 +0530

    Update main.tf

--- modules/aws_S3/main.tf
+++ modules/aws_S3/main.tf
@@ -1,14 +1,14 @@
-resource "aws_s3_bucket" "example" {
-  bucket = "example-bucket"
+resource "aws_s3_bucket" "my-bucket1507" {
+  bucket = "my-bucket1507"
 }
 
-resource "aws_s3_bucket_acl" "example" {
-  bucket = aws_s3_bucket.example.id
-  acl    = "private"
+resource "aws_s3_bucket_acl" "my-bucket1507" {
+  bucket = aws_s3_bucket.my-bucket1507.id
+  acl    = "public"
 }
 
 resource "aws_s3_bucket_versioning" "versioning_example" {
-  bucket = aws_s3_bucket.example.id
+  bucket = aws_s3_bucket.my-bucket1507.id
   versioning_configuration {
     status = "Enabled"
   }
