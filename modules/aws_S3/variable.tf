dc03e7ba1b3c7631d3ea9c44c69b71321b8b80bb
ajaypatil352 <117426390+ajaypatil352@users.noreply.github.com>
2023-07-15 16:54:13 +0530

    Update variable.tf

--- modules/aws_S3/variable.tf
+++ modules/aws_S3/variable.tf
@@ -1,4 +1,17 @@
-variable "bucket"{
-  type = string
-  description = "this is S# bucket name"
+variable "bucket_name" {
+  description = "Name of the S3 bucket"
+  type        = string
+  default     = "my-bucket1507"
+}
+
+variable "bucket_acl" {
+  description = "ACL for the S3 bucket"
+  type        = string
+  default     = "public"
+}
+
+variable "versioning_status" {
+  description = "Status of bucket versioning"
+  type        = string
+  default     = "Enabled"
 }
