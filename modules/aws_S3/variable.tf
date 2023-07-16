e36582a36bec6ffa8e83648ab052d07811a6cab3
ajaypatil352 <117426390+ajaypatil352@users.noreply.github.com>
2023-07-16 21:18:41 +0530

    Update variable.tf

--- modules/aws_S3/variable.tf
+++ modules/aws_S3/variable.tf
@@ -1,19 +1,19 @@
 variable "bucket_name" {
   description = "Name of the S3 bucket"
   type        = string
-  default     = "my-bucket1507"
+  default     = "my-bucket1508"
 }
 
-#variable "bucket_acl" {
- # description = "ACL for the S3 bucket"
-  #type        = string
+variable "bucket_acl" {
+  description = "ACL for the S3 bucket"
+  type        = string
   #default     = "private"
-#}
+}
 
-#variable "versioning_status" {
-#  description = "Status of bucket versioning"
-#  type        = string
-#  default     = "Enabled"
-#}
+variable "versioning_status" {
+  description = "Status of bucket versioning"
+  type        = string
+  default     = "Enabled"
+}
 variable "aws_access_key" {}
 variable "aws_secret_key" {}
