2e1cb01acd6adc5c65b8ddc178e68871932e8142
ajaypatil352 <117426390+ajaypatil352@users.noreply.github.com>
2023-07-16 21:17:18 +0530

    Update main.tf

--- modules/aws_S3/main.tf
+++ modules/aws_S3/main.tf
@@ -2,10 +2,10 @@ resource "aws_s3_bucket" "my-bucket1507" {
   bucket = var.bucket_name
 }
 
-resource "aws_s3_bucket_acl" "my-bucket1507" {
-  bucket = aws_s3_bucket.my-bucket1507.id
-  acl    = var.bucket_acl
-}
+#resource "aws_s3_bucket_acl" "my-bucket1507" {
+#  bucket = aws_s3_bucket.my-bucket1507.id
+ # acl    = var.bucket_acl
+#}
 
 resource "aws_s3_bucket_versioning" "versioning_example" {
   bucket = aws_s3_bucket.my-bucket1507.id
