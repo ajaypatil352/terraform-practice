83d0ed3a7d3d4bcfa7f8bfc542f5a60ecc43f2e6
ajaypatil352 <117426390+ajaypatil352@users.noreply.github.com>
2023-07-18 14:38:48 +0530

    Update variable.tf

--- modules/rds/variable.tf
+++ modules/rds/variable.tf
@@ -1,1 +1,37 @@
+variable "allocated_storage"{
+  type        = number
+  description = "this is allocated_storage"
+}
+
+variable "db_name"{
+  type        = string
+  description = "this is db name"
+}
+variable "engine"{
+  type        = string
+  description = "this version of db"
+}
+
+variable "engine_version"{
+  type        = number
+  description = "this is engine version"
+}
+variable "instance_class"{
+  type        = string
+  description = "this is instance class"
+}
+variable "username"{
+  type        = string
+  description = "this user name"
+}
+variable "password"{
+  type        = string
+  description = "this is password"
+}
+variable "parameter_group_name"{
+  type        = string
+  description = "this is parameter group"
+}
+
+
 
