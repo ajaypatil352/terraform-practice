3a1375a78c8eef0ea945ef3495f6df1e637aebcc
ajaypatil352 <117426390+ajaypatil352@users.noreply.github.com>
2023-07-18 13:54:26 +0530

    Delete modules/key directory

--- modules/key/output.tf
+++
@@ -1,4 +0,0 @@
-output "private_key" {
-  description = "Private key of the key pair"
-  value       = aws_key_pair.my_key_pair.private_key
-}
--- modules/key/terraform.auto.tfvars
+++
@@ -1,2 +0,0 @@
-key_pair_name   = "my-key-pair"
-public_key_path = "~/.ssh/id_rsa.pub"
