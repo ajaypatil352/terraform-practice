500e9cf533e8bf32378280fbb4de7533792414e2
ajaypatil352 <117426390+ajaypatil352@users.noreply.github.com>
2023-07-18 21:15:55 +0530

    Update main.tf

--- modules/aws_S3/main.tf
+++ modules/aws_S3/main.tf
@@ -1,14 +1,14 @@
-resource "aws_s3_bucket" "my-bucket1508" {
+resource "aws_s3_bucket" "my-bucket1808" {
   bucket = var.bucket_name
 }
 
-resource "aws_s3_bucket_acl" "my-bucket1508" {
+resource "aws_s3_bucket_acl" "my-bucket1808" {
   bucket = aws_s3_bucket.my-bucket1508.id
   acl    = var.bucket_acl
 }
 
 resource "aws_s3_bucket_versioning" "versioning_example" {
-  bucket = aws_s3_bucket.my-bucket1508.id
+  bucket = aws_s3_bucket.my-bucket1808.id
   versioning_configuration {
     status = "Enabled"
   }
