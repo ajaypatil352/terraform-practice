58ae7964af549d368d524dc7f46c5aed8857eb03
ajaypatil352 <117426390+ajaypatil352@users.noreply.github.com>
2023-07-18 19:21:55 +0530

    Update output.tf

--- modules/rds/output.tf
+++ modules/rds/output.tf
@@ -1,1 +1,9 @@
-####
+output "rds_arn" {
+  description = "ARN of the Application Load Balancer"
+  value       = aws_db_instance.mydb.arn
+}
+
+output "allocated_storage" {
+  description = "ARN of the Application Load Balancer"
+  value       = aws_db_instance.mydb.allocated_storage
+}
