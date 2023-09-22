5c622badb099c71c06540dcb10bad5c455fa427e
ajaypatil352 <117426390+ajaypatil352@users.noreply.github.com>
2023-09-22 12:30:54 +0530

    Update main.tf

--- modules/sns/main.tf
+++ modules/sns/main.tf
@@ -1,1 +1,20 @@
+resource "aws_sns_topic" "aws_sns" {
+  name = "sns_topic"
+  display_name = 
+  fifo_topic    = true
+  content_based_deduplication = true
+  kms_master_key_id 
+  policy 
+  delivery_policy
+  kms_data_key_reuse_period_seconds = var.sns_kms_data_key_reuse_period_seconds
+  application_success_feedback_role_arn = var.sns_application_success_feedback_role_arn
+  application_success_feedback_sample_rate = var.sns_application_success_feedback_sample_rate
+  application_failure_feedback_role_arn = var.sns_application_failure_feedback_role_arn
+  application_failure_feedback_sample_rate = var.sns_application_failure_feedback_sample_rate
+  http_success_feedback_role_arn = var.sns_http_success_feedback_role_arn
+  http_success_feedback_sample_rate = var.sns_http_success_feedback_sample_rate
+  http_failure_feedback_role_arn = var.sns_http_failure_feedback_role_arn
+  http_failure_feedback_sample_rate = var.sns_http_failure_feedback_sample_rate
+  tags                      = var.sns_tags
+}
 
